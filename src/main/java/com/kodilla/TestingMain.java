a15af8067199cfe01622c02b7629e2ef258dc365
magdasikl <msiklucka@gmail.com>
2018-04-12 22:04:04 +0200

    3 commit

--- src/main/java/com/kodilla/TestingMain.java
+++ src/main/java/com/kodilla/TestingMain.java
@@ -2,6 +2,8 @@ package com.kodilla;
 
 public class TestingMain {
     public static void main(String[] args){
+
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramownia");
+
     }
 }
--- src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,4 +1,11 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
+    public double add(int a, int b){
+        return a + b;
+    }
+
+    public double subtract(int a, int b){
+        return a - b;
+    }
 }
