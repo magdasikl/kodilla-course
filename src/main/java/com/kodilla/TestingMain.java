5f8d48c2a57777d5453565549d13296c4d39734c
magdasikl <msiklucka@gmail.com>
2018-04-13 20:20:49 +0200

    add metod test calculator

--- src/main/java/com/kodilla/TestingMain.java
+++ src/main/java/com/kodilla/TestingMain.java
@@ -1,9 +1,25 @@
 package com.kodilla;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
 
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramownia");
+        Calculator simpleCalculator = new Calculator();
+        int addResult = simpleCalculator.add(18, 150);
+        int substractResult = simpleCalculator.subtract(1350, 879);
+
+        if (addResult == 168) {
+            System.out.println("Dodawanie zgadza się");
+        } else {
+            System.out.println("Błąd dodawania");
+        }
+
+        if (substractResult == 472){
+            System.out.println("Odejmowanie jest zgodne");
+        } else {
+            System.out.println("Błąd odejmowania");
+        }
 
     }
 }
