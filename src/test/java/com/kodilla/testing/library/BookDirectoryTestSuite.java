215a70a34ef7e82b0f2d68daa7d8216fec1cee59
magdasikl <msiklucka@gmail.com>
2018-04-22 21:22:43 +0200

    Dodanie importu do BookDirectoryTestSuite

--- src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
+++ src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
@@ -5,11 +5,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
-//import static org.mockito.ArgumentMatchers.anyString;
-//import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
