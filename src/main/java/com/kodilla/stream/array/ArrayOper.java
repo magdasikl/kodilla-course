cba946e8d8b6095b6f08f7db0f1cf4778b1b7599
magdasikl <msiklucka@gmail.com>
2018-07-30 10:46:46 +0200

    Poprawa zadania 7.5

--- src/main/java/com/kodilla/stream/array/ArrayOper.java
+++ src/main/java/com/kodilla/stream/array/ArrayOper.java
@@ -1,13 +1,13 @@
 package com.kodilla.stream.array;
 
 import java.util.OptionalDouble;
-import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 public class ArrayOper implements ArrayOperations {
-    public OptionalDouble getAverage(int[] numbers){
+    public  OptionalDouble getAverage(int[] numbers){
 
        IntStream.range(0,numbers.length)
+                .map(operand -> numbers[operand])
                 .forEach(System.out::println);
 
         OptionalDouble averageNumbers = IntStream.range(0,numbers.length)
--- src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,10 +1,8 @@
 package com.kodilla.stream.array;
 
 import java.util.OptionalDouble;
-import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     public OptionalDouble getAverage(int[] numbers);
-
 
 }
